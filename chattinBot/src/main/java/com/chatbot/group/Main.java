ba02d21525846fff14daca66f732cc2d28b2b32f
Danylo.Ivzhenko <ivzhenkodanil@gmail.com>
2023-01-28 23:47:09 +0200

    First commit

---
+++ chattinBot/src/main/java/com/chatbot/group/Main.java
@@ -0,0 +1,11 @@
+package com.chatbot.group;
+
+/**
+ * Задача: написать чатбота для Telegram. Сохранять информацию о пользователях в БД и показывать статистику. Так же
+ * сохрянять очки пользователей в БД и по вызову показывать их в лидере топе.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
